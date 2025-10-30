083557846c8383fba3aec7ce961829a3c63d1341
Chaker Saidi <152811430+chaker88@users.noreply.github.com>
2025-10-30 10:21:44 +0000

    update  my files

--- regular_expression_cheasheet.md
+++ regular_expression_cheasheet.md
@@ -6,3 +6,17 @@
     ? 0 or 1 repetition
     {m} repetitions
     {m,n} m-n repetitions
+    ^ match the starts of a string 
+    $ match the ends of a string 
+    [] set of characters
+    [^] complementing the set
+    [a-zA-Z] letters
+    [0-9] numbers
+    \w alpha-numeric and the  _ underscore
+    \W not a word character
+    \d decimal digit
+    \D not a decimal
+    \s whitespace character
+    \S not a whitespace
+
+
--- validate.py
+++ validate.py
@@ -6,12 +6,12 @@ import re
 email = input("Enter your email address: ")
 
 
-pattern = r'.+@.+\.edu' # to escape the dot we use \. because dot means any character in regex
+pattern = r'^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$' # to escape the dot we use \. because dot means any character in regex
+pattern = r'^[\w]+@[\w]+\.edu$' # the same as above but using \w which means alphanumeric characters plus underscore
 
-
-if re.search(pattern,email):
+if re.search(pattern,email,flags=re.IGNORECASE): # IGNORECASE to make it case insensitive
     print("Valid email address.")
 else:
     print("Invalid email address.")
 
-# I stopped at 30:00 minutes
+# I stopped at 01:00:00 in the lecture video
